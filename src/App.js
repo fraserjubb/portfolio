0b27cb04dd221164c0a8300dcdc2cd3428a48697
Fraser Jubb <fraser.jubb@gmail.com>
2023-01-27 14:44:58 +0000

    Simple test to make sure it works without properly updating the public version.

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Testing that this is actually working.
         </p>
         <a
           className="App-link"
