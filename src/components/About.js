a6db9b180dabaaacdb55dd483e23f2267e92b64d
Fraser Jubb <fraser.jubb@gmail.com>
2023-01-28 15:46:09 +0000

    Same as before.

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,14 @@
+import profilePic from './images/profilepic.png'
+
 const About = () => {
     return (
     <>
     <h1>ABOUT ME</h1>
     <p>I'm a game developer currently based in Edinburgh, Scotland.</p>
+    <img
+    src={profilePic}
+    width = "300"
+    ></img>
     </>
 )}
 
