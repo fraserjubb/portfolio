a6f0c5803903763c0bfe8d2fefc3af777336d057
Fraser Jubb <fraser.jubb@gmail.com>
2023-01-28 12:00:15 +0000

    Added LinkedIn and made it so that links open in new tabs.

--- src/components/SocialMediaNavBar.js
+++ src/components/SocialMediaNavBar.js
@@ -1,25 +1,28 @@
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faFacebook, faGithub, faInstagram, faTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons'
+import {faFacebook, faGithub, faInstagram, faLinkedin, faTwitter, faYoutube} from '@fortawesome/free-brands-svg-icons'
 
 const SocialMediaNavBar = () => {
     return(
         <>
         <h4>Socials</h4>
         <div>
-            <a href="https://github.com/fraserjubb">
+            <a href="https://github.com/fraserjubb" target="_blank">
                 <FontAwesomeIcon icon={faGithub}/>
             </a>
-            <a href="https://instagram.com/thejubbzone">
+            <a href="https://instagram.com/thejubbzone" target="_blank">
                 <FontAwesomeIcon icon={faInstagram}/>
             </a>
-            <a href="https://facebook.com/thejubbzone">
+            <a href="https://facebook.com/thejubbzone" target="_blank">
                 <FontAwesomeIcon icon={faFacebook}/>
             </a>
-            <a href="https://twitter.com/fraserjubb">
+            <a href="https://twitter.com/fraserjubb" target="_blank">
                 <FontAwesomeIcon icon={faTwitter}/>
             </a>
-            <a href="https://www.youtube.com/@thejubbzone2374/featured"> 
+            <a href="https://www.youtube.com/@thejubbzone2374/featured" target="_blank"> 
             <FontAwesomeIcon icon={faYoutube}/> 
+            </a>
+            <a href="https://www.linkedin.com/in/fraser-jubb-008593103" target="_blank"> 
+            <FontAwesomeIcon icon={faLinkedin}/> 
             </a>
         </div>
 
